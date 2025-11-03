300ff62aa851c707e2ff70c14782961addff3e2c
Ethan <ethanhchen01@gmail.com>
2025-11-03 00:11:12 -0600

    added check in Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main (String[] args) {
+        if (args.length != 1) {
+            System.out.println("usage: java Main config-file");
+            return;
+        }
         Node n = ConfigParser.parse(args[0]);
         System.out.println(n.toString());
         // n.establishConnections();
