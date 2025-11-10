751e97716797fe3154d2563fe3492c8621a5de1f
Ethan <ethanhchen01@gmail.com>
2025-11-10 01:14:36 -0600

    working on socket connections in Node

--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Arrays;
 
 public class Node {
@@ -8,9 +14,43 @@ public class Node {
     int csExecutionTime;
     int numRequests;
     int[] qMembers;
+    ServerSocket serverSocket;
+    Connection[] clientSockets;
+
+    private boolean listen() {
+        int numConnected = 0;
+        try {
+            this.serverSocket = new ServerSocket(this.port);
+            long start = System.currentTimeMillis();
+            while (numConnected < clientSockets.length) {
+                Socket client = serverSocket.accept(); 
+                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+                clientSockets[Integer.parseInt(in.readLine())] = new Connection(client, in, out); //clientSockets[node_number], connecting client must send their node number once accepted
+
+                if (System.currentTimeMillis() - start > 15000) { //if timeout, then close all connections, exit
+                    for (int i = 0; i < clientSockets.length; i++) {
+                        clientSockets[i].close();
+                    }
+                    return false;
+                }
+                numConnected++;
+            }
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    private boolean connect() {
+        
+    }
 
     public void establishConnections() {
-
+        listen();
+        connect();
     }
     
     public void beginProtocol() {
@@ -18,7 +58,6 @@ public class Node {
     }
 
     private String lt() { return "\n\t"; }
-
     @Override
     public String toString() {
         return "Node {\n\t" +
